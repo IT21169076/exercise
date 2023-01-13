4053934ff8db01808335a48c0ea91713caa696d2
Shalini Muthumali Athukorala <99632841+IT21169076@users.noreply.github.com>
2023-01-13 08:24:00 +0530

    Display uppercased alphabet using for loop

---
+++ uppercased.java
@@ -0,0 +1,13 @@
+code
+class Main {
+  public static void main(String[] args) {
+
+    char c;
+
+    for(c = 'A'; c <= 'Z'; ++c)
+      System.out.print(c + " ");
+    }
+}
+
+output
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 
