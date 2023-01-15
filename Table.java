6a50c5676b29f00adb146705800277c74b29d67f
Shalini Muthumali Athukorala <99632841+IT21169076@users.noreply.github.com>
2023-01-15 09:33:25 +0530

    Generate Multiplication Table using for loop

---
+++ Table.java
@@ -0,0 +1,26 @@
+CODE 
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+
+        int num = 5;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+}
+
+OUTPUT
+
+5 * 1 = 5
+5 * 2 = 10
+5 * 3 = 15
+5 * 4 = 20
+5 * 5 = 25
+5 * 6 = 30
+5 * 7 = 35
+5 * 8 = 40
+5 * 9 = 45
+5 * 10 = 50
