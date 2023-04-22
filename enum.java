8f766de312366bf22165535901ff3b6df2c0b19d
Shalini Muthumali Athukorala <99632841+IT21169076@users.noreply.github.com>
2023-04-22 13:00:30 +0530

    Java Enums

---
+++ enum.java
@@ -0,0 +1,18 @@
+enum Level {
+  LOW,
+  MEDIUM,
+  HIGH
+}
+
+public class Main { 
+  public static void main(String[] args) { 
+    Level myVar = Level.MEDIUM; 
+    System.out.println(myVar); 
+  } 
+}
+
+
+
+output 
+
+Hello World
